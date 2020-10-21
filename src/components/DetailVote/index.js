dd4462a339a1743d0d78a35da94cd2990539dba5
parhan <apriliandi642@gmail.com>
2020-10-22 00:32:39 +0700

    add socket io for realtime data for client

--- src/components/DetailVote/index.js
+++ src/components/DetailVote/index.js
@@ -1,25 +1,47 @@
 import React from "react";
-import { useQuery } from "react-query";
+import axios from "axios";
+import socketIOClient from "socket.io-client";
 import Navbar from "../Navbar";
+import Spinner from "../Spinner";
 import "./css/style.css";
 
-async function getDetailVote(key, id) {
-   const result = await fetch(`http://localhost:4000/api/votes/${id}`);
-   return result.json();
-}
+const socket = socketIOClient("http://localhost:4000/");
 
 export default function DetailVote({ match }) {
-   const { data: vote, status } = useQuery(
-      ["vote", match.params.id],
-      getDetailVote
+   const [vote, isLoading, setVote] = useFetchVote(
+      `http://localhost:4000/api/votes/${match.params.id}`
    );
 
-   if (status === "error") {
-      return <h1>Something error</h1>;
+   socket.on("voting", (data) => {
+      if (data._id !== match.params.id) {
+         return;
+      }
+
+      setVote(data);
+   });
+
+   function objVoted(object) {
+      const copy = { ...vote };
+
+      if (object === "obj1") {
+         copy.object1.totalVotes = copy.object1.totalVotes + 1;
+      } else {
+         copy.object2.totalVotes = copy.object2.totalVotes + 1;
+      }
+
+      socket.emit("voting", [copy, match.params.id]);
+
+      socket.on("voting", (data) => {
+         if (data._id !== match.params.id) {
+            return;
+         }
+
+         setVote(data);
+      });
    }
 
-   if (status === "loading") {
-      return <h1>Loading</h1>;
+   if (isLoading === true || vote === "") {
+      return <Spinner />;
    }
 
    return (
@@ -39,7 +61,9 @@ export default function DetailVote({ match }) {
                      value={vote.object1.totalVotes}
                      max={vote.maximumVote}
                   ></progress>
-                  <button className="btn-vote">Voting</button>
+                  <button onClick={() => objVoted("obj1")} className="btn-vote">
+                     Voting
+                  </button>
                </div>
 
                <div className="object-2">
@@ -49,10 +73,50 @@ export default function DetailVote({ match }) {
                      value={vote.object2.totalVotes}
                      max={vote.maximumVote}
                   ></progress>
-                  <button className="btn-vote">Voting</button>
+                  <button onClick={() => objVoted("obj2")} className="btn-vote">
+                     Voting
+                  </button>
                </div>
             </div>
          </div>
       </React.Fragment>
    );
+}
+
+function useFetchVote(endPoint) {
+   const CancelToken = axios.CancelToken;
+   const [isLoading, setLoading] = React.useState(false);
+   const [vote, setVote] = React.useState("");
+
+   React.useEffect(() => {
+      let cancel;
+
+      async function fetchData() {
+         setLoading(true);
+
+         try {
+            const result = await axios(endPoint, {
+               method: "GET",
+               cancelToken: new CancelToken((c) => {
+                  cancel = c;
+               }),
+            });
+
+            setVote(result.data);
+         } catch (error) {
+            console.log("something error", error);
+         }
+
+         setLoading(false);
+      }
+
+      fetchData();
+
+      return () => {
+         setVote();
+         cancel();
+      };
+   }, [endPoint, CancelToken]);
+
+   return [vote, isLoading, setVote];
 }
