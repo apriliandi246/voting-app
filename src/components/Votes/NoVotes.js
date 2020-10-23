3d14749c8fd1162cb8e6f7a096ba7cc8e53f3996
parhan <apriliandi642@gmail.com>
2020-10-24 02:49:32 +0700

    add no vote component and update some styling

---
+++ src/components/Votes/NoVotes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NoVotes() {
+   return <h1 className="no-votes">No Votes Right Now 😅</h1>;
+}
--- src/components/Votes/index.js
+++ src/components/Votes/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import Spinner from "../Spinner";
 import Vote from "./Vote";
+import NoVotes from "./NoVotes";
 
 async function getAllVotes() {
    const result = await axios("http://localhost:4000/api/votes");
@@ -14,6 +15,10 @@ export default function Votes() {
 
    if (status === "loading") {
       return <Spinner />;
+   }
+
+   if (status === "success" && votes.length === 0) {
+      return <NoVotes />;
    }
 
    return (
